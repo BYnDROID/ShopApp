109488eea280ea6b5330efd0928388b76856ebe7
BYnDROID <bulentyildiz07@gmail.com>
2020-07-16 19:51:07 +0300

    Raouting İmplemation Cart ve Checkout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ShopModule } from './shop/shop.module';
+import { RouterModule } from '@angular/router';
+import { ShopComponent } from './shop/shop.component';
+import { CartDetailComponent } from './shop/cart-detail/cart-detail.component';
+import { CheckoutComponent } from './shop/checkout/checkout.component';
 
 @NgModule({
-  declarations: [ AppComponent ],
-  imports: [ BrowserModule, ShopModule ],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ShopModule,
+    RouterModule.forRoot([
+      { path: 'shop', component: ShopComponent },
+      { path: 'cart', component: CartDetailComponent },
+      { path: 'checkout', component: CheckoutComponent },
+      { path: '**', redirectTo: "/shop" }
+    ])
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -5,11 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { ShopComponent } from './shop.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CartSummaryComponent } from './cart-summary/cart-summary.component';
+import { CartDetailComponent } from './cart-detail/cart-detail.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+
 
 @NgModule({
   imports: [ModelModule, BrowserModule, FormsModule],
-  declarations: [ShopComponent, NavbarComponent, CartSummaryComponent],
-  exports: [ShopComponent]
+  declarations: [ShopComponent, NavbarComponent, CartSummaryComponent, CartDetailComponent, CheckoutComponent],
+  exports: [ShopComponent, CartDetailComponent, CheckoutComponent]
 })
 export class ShopModule{
 
