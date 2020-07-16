1404d51a219005fe4d0d4fb0494ee6ed7ea2c57f
BYnDROID <bulentyildiz07@gmail.com>
2020-07-16 13:08:02 +0300

    Repository için json-server data REST

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShopModule } from './shop/shop.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [ AppComponent ],
+  imports: [ BrowserModule, ShopModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
