72efa020f4ad96f8447e9326fdaafa2111f230f5
BYnDROID <bulentyildiz07@gmail.com>
2020-07-17 20:52:34 +0300

    Alışveriş Sipariş Tamamlama

--- data.js
+++ data.js
@@ -7,13 +7,18 @@ module.exports = function(){
       { id: 3, name: 'Samsung S3' , price: 3000, imageUrl: '3.jpg', description: 'İdere eder 3' , category: 'Telefon' },
       { id: 4, name: 'Samsung S4' , price: 4000, imageUrl: '4.jpg', description: 'İdere eder 4' , category: 'Telefon' },
       { id: 5, name: 'Samsung S5' , price: 5000, imageUrl: '5.jpg', description: 'İdere eder 5' , category: 'Telefon' },
-      { id: 6, name: 'ASUS I7', price: 6000, imageUrl: '6.jpg', description: 'Güzeeeel MAkina' , category: 'Bilgisayar' },
-      { id: 7, name: 'HP I9', price: 7000, imageUrl: '7.jpg', description: 'Harika MAKİNAAAAAAAA' , category: 'Bilgisayar' }
+      { id: 6, name: 'Samsung S6' , price: 6000, imageUrl: '6.jpg', description: 'İdere eder 6' , category: 'Telefon' },
+      { id: 7, name: 'Samsung S7' , price: 7000, imageUrl: '7.jpg', description: 'İdere eder 7' , category: 'Telefon' },
+      { id: 8, name: 'ASUS I5'    , price: 6000, imageUrl: 'b1.jpg', description: 'Güzeeeel MAkina' , category: 'Bilgisayar' },
+      { id: 9, name: 'HP I7'      , price: 7000, imageUrl: 'b2.jpg', description: 'Harika MAKİNAAAAAAAA' , category: 'Bilgisayar' },
+      { id: 10, name: 'DELL I9'    , price: 7000, imageUrl: 'b3.jpg', description: 'Harika MAKİNAAAAAAAA' , category: 'Bilgisayar' },
+      { id: 11, name: 'Excalibur I7' , price: 7000, imageUrl: 'l1.jpg', description: 'Harika Laptop MAKİNAAAAAAAA' , category: 'Laptop' },
+      { id: 12, name: 'Excalibur I9' , price: 11000, imageUrl: 'l2.jpg', description: 'Harika Laptop MAKİNAAAAAAAA' , category: 'Laptop' }
     ],
     categories: [
       { id: 1, name: 'Telefon' },
       { id: 2, name: 'Bilgisayar' },
-      { id: 3, name: 'Elektronik' }
+      { id: 3, name: 'Laptop' }
     ],
     orders: [
 
