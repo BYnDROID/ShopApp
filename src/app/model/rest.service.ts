78c9a7b6314c08a5ddc6fcb687aae448a8f9283d
BYnDROID <bulentyildiz07@gmail.com>
2020-07-18 19:00:46 +0300

    JWT ile Authentication

--- src/app/model/rest.service.ts
+++ src/app/model/rest.service.ts
@@ -4,13 +4,15 @@ import { Observable } from 'rxjs';
 import { Product }    from './product.model';
 import { Category } from './category.model';
 import { Order } from './order.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestService {
 
-  baseUrl :string = "http://localhost:3500/";
+  baseUrl: string = "http://localhost:3500/";
+  token: string  = "";
 
   constructor(private http: HttpClient) { }
 
@@ -25,4 +27,16 @@ export class RestService {
   saveOrder(order: Order):Observable<Order> {
     return this.http.post<Order>(this.baseUrl + 'orders', order);
   }
+
+  authentication(username: string, password: string):Observable<boolean> {
+    return this.http.post<any>(this.baseUrl + 'ligin', {
+      username:username,
+      pasword:password
+    }).pipe( map( response => {
+      this.token = response.success ? response.token : null;
+      console.log(this.token);
+      return response.success;
+    } ) );
+  }
+
 }
