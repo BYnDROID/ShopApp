46dbf0c379ca4446b519a4ce1d40af89fdf588b4
BYnDROID <bulentyildiz07@gmail.com>
2020-07-20 15:46:26 +0300

    Yönetici Sayfalarının Eklenmesi

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,12 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { AuthComponent } from './auth/auth.component';
-import { AdminComponent } from './admin.component';
-import { AutGuard } from './auth.guard';
+import { NgModule }               from '@angular/core';
+import { Routes, RouterModule }   from '@angular/router';
+
+import { AuthComponent }          from './auth/auth.component';
+import { AdminComponent }         from './admin.component';
+import { AutGuard }               from './auth.guard';
+import { ProductFormComponent }   from './products/product-form/product-form.component';
+import { ProductListComponent }   from './products/product-list/product-list.component';
+import { CategoryFormComponent }  from './categories/category-form/category-form.component';
+import { CategoryListComponent }  from './categories/category-list/category-list.component';
+import { OrderListComponent }     from './orders/order-list/order-list.component';
 
 const routes: Routes = [
   {path: 'auth', component: AuthComponent},
-  {path: 'main', component: AdminComponent, canActivate:[AutGuard]},
+  {path: 'main', component: AdminComponent, canActivate:[AutGuard],
+    children: [
+      {path: 'products/:mode/:id'   , component: ProductFormComponent  },
+      {path: 'products/:mode/'      , component: ProductFormComponent  },
+      {path: 'products'             , component: ProductListComponent  },
+      {path: 'categories/:mode/:id' , component: CategoryFormComponent },
+      {path: 'categories/:mode/'    , component: CategoryFormComponent },
+      {path: 'categories'           , component: CategoryListComponent },
+      {path: 'orders'               , component: OrderListComponent }
+    ]},
   {path: '**', redirectTo: 'auth'}
 ];
 
